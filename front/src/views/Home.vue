01945df7133eeee251b7652e0c20cb96f61c2c26
morifuji.koudai <marooon88@gmail.com>
2019-01-26 11:01:02 +0900

    [add] サーバーとの仕様決定

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Primary title
+          </h1>
+          <h2 class="subtitle">
+            Primary subtitle
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns is-mobile">
+          <div v-for="image in images" :key="image.id" class="column is-half">
+            <img src="https://bulma.io/images/placeholders/480x480.png">
+          </div>
+          <div class="column is-half">
+            <img src="https://bulma.io/images/placeholders/480x640.png">
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      images: [],
+      
+    }
   }
 }
 </script>
